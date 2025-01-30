819dd2288440cbb17a39b763d82016f202b0d3f7
Pr1yansu <priyansuchowdhury57@gmail.com>
2025-01-31 00:54:14 +0530

    Check out the next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "https://runtime-project-management-tool-production.up.railway.app/api/:path*", // Proxy to Backend
+      },
+    ];
+  },
+};
 
 export default nextConfig;
