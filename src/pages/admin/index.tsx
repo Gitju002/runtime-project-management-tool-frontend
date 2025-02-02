5892296aadfd7f65259945adb4df480d42220aa2
Snehashis Gharai <connectsnehashis22@gmail.com>
2025-02-02 21:08:12 +0530

    Enhance Admin Dashboard layout and styling with responsive design and new card components

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -2,41 +2,47 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Admin() {
   return (
-    <div className="space-y-4 border border-red-500 ">
-      <h1 className="text-3xl font-bold ">Welcome to the Admin Dashboard</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-        <Card>
-          <CardHeader>
-            <CardTitle>Total Projects</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p className="text-4xl font-bold">24</p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Active Services</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p className="text-4xl font-bold">12</p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Project Types</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p className="text-4xl font-bold">8</p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Total Users</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <p className="text-4xl font-bold">156</p>
-          </CardContent>
-        </Card>
+    <div className="container  mx-auto min-h-screen w-full py-6">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="md:text-2xl font-bold ">
+          Welcome to the Admin Dashboard
+        </h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 ">
+        {[
+          "Total Users",
+          "Initiated Projects",
+          "Ongoing Projects",
+          "Completed Projects",
+        ].map((title, index) => (
+          <Card key={index} className="card-dark">
+            <CardHeader>
+              <CardTitle className="dark:text-teal-950 md:text-lg">
+                {title}
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="text-end text-6xl font-semibold">
+                {index === 0
+                  ? "24"
+                  : index === 1
+                  ? "12"
+                  : index === 2
+                  ? "8"
+                  : "156"}
+              </p>
+              {/* <p>
+                {index === 0
+                  ? "Users"
+                  : index === 1
+                  ? "Projects"
+                  : index === 2
+                  ? "Projects"
+                  : "Projects"}
+              </p> */}
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </div>
   );
