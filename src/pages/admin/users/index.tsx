4493946b70c4fe142c6400003bf2a9c92ddc67cb
Purbarun <purbarunmondal@gmail.com>
2025-02-10 18:48:01 +0530

    Enhance user analytics page with user cards and tooltips; update pagination limit and refactor chart data handling

--- src/pages/admin/users/index.tsx
+++ src/pages/admin/users/index.tsx
@@ -6,6 +6,21 @@ import { columns } from "../../../components/table/table-columns/admin-users-col
 import { useGetAllUsersQuery } from "@/store/api/user";
 import { CustomPagination } from "@/components/ui/custom-pagination";
 import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "@/components/ui/tooltip";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ChartArea } from "lucide-react";
 
 export default function Users() {
   const router = useRouter();
@@ -13,7 +28,7 @@ export default function Users() {
 
   const [userSearch, setUserSearch] = useState(""); // Debounced search state
   const currentPage = Number(searchParams.get("page")) || 1;
-  const [limit, setLimit] = useState(10);
+  const [limit, setLimit] = useState(12);
   const [totalPages, setTotalPages] = useState(1);
 
   const {
@@ -26,6 +41,18 @@ export default function Users() {
     page: currentPage,
     limit,
   });
+
+  const handlePageChange = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push(`?${params.toString()}`);
+  };
+
+  const handleClick = (userName: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("userName", userName);
+    router.push(`/admin/analytics?${params.toString()}`);
+  };
 
   // Debounced Effect for Search Input (3-second delay)
   useEffect(() => {
@@ -42,12 +69,6 @@ export default function Users() {
 
     return () => clearTimeout(delay);
   }, [userSearch]);
-
-  const handlePageChange = (page: number) => {
-    const params = new URLSearchParams(searchParams);
-    params.set("page", page.toString());
-    router.push(`?${params.toString()}`);
-  };
 
   useEffect(() => {
     if (userData) {
@@ -70,11 +91,67 @@ export default function Users() {
         </div>
       </div>
       <>
-        <DataTable
+        {/* <DataTable
           columns={columns}
           isLoading={isLoading || isFetching}
           data={isError ? [] : userData?.data?.users || []}
-        />
+        /> */}
+        <div className="my-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {userData?.data?.users.map((user) => (
+            <Card key={user.id} className="border-2 border-lime-shade/30">
+              <CardHeader>
+                <CardTitle className="flex items-center justify-between gap-4">
+                  <div className="flex items-center gap-4">
+                    <Image
+                      height={40}
+                      width={40}
+                      src={user.profilepic}
+                      alt={user.name}
+                      className="rounded-full h-16 w-16 obcject-cover ring-2 ring-teal-shade"
+                    />
+                    <h3 className="text-lg font-bold">{user.name}</h3>
+                  </div>
+
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <div
+                        className="cursor-pointer hover:underline "
+                        onClick={() => handleClick(user.name)}
+                      >
+                        <ChartArea className="size-8 stroke-1 stroke-teal-shade" />
+                      </div>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Click to view analytics</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <CardDescription className="space-y-4">
+                  <section className="grid grid-cols-2 gap-2">
+                    <div>
+                      <p className="text-sm font-bold">Departnent:</p>
+                      <p>{user.department_name}</p>
+                    </div>
+                    <div>
+                      <p className="text-sm font-bold">Mobile:</p>
+                      <p>{user.mobile || "N/A"}</p>
+                    </div>
+                    <div>
+                      <p className="text-sm font-bold">Office:</p>
+                      <p>{user.office_name}</p>
+                    </div>
+                    <div>
+                      <p className="text-sm font-bold">Role:</p>
+                      <p>{user.role_name || "N/A"}</p>
+                    </div>
+                  </section>
+                </CardDescription>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
         <CustomPagination
           currentPage={currentPage}
           totalPages={totalPages}
